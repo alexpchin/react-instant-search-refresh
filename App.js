b0a95037c7b706dd1ce2edd19d6db4a98805e1a9
Alex Chin <alex@alexchin.co.uk>
2021-01-23 10:37:10 +0000

    Setup

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useMemo, useCallback, useState } from "react";
+import {
+  StyleSheet,
+  Pressable,
+  FlatList,
+  Image,
+  Dimensions,
+} from "react-native";
+import algoliasearch from "algoliasearch";
+import { connectInfiniteHits, InstantSearch } from "react-instantsearch-native";
+
+const { width } = Dimensions.get("window");
+
+const searchClient = algoliasearch(
+  "latency",
+  "6be0576ff61c053d5f9a3225e2a90f76"
+);
+
+const photosIndex = searchClient.initIndex("photos");
+
+const Hits = ({ hasMore, refine, hits, refreshing, onRefresh }) => {
+  const _onEndReached = () => {
+    if (hasMore) {
+      refine();
+    }
+  };
+
+  // message: "Not enough rights to delete an object near line:1 column:68"
+  const _deleteObject = async (id) => {
+    try {
+      await photosIndex.deleteObject(id).wait();
+      // Trigger refresh
+      onRefresh();
+    } catch (e) {
+      console.log("Error deleting object", e);
+    }
+  };
+
+  const _renderItem = ({ item: hit }) => (
+    <Pressable onPress={() => _deleteObject(hit.objectID)} style={styles.item}>
+      <Image
+        style={{ height: width / 3, width: width / 3 }}
+        source={{ uri: hit.image }}
+      />
+    </Pressable>
+  );
+
+  const _keyExtractor = useCallback((_, index) => index.toString(), []);
+
+  return (
+    <FlatList
+      data={hits}
+      extraData={hits}
+      numColumns={3}
+      renderItem={_renderItem}
+      keyExtractor={_keyExtractor}
+      onEndReached={_onEndReached}
+      refreshing={refreshing}
+      onRefresh={onRefresh}
+    />
+  );
+};
+
+const ConnectedHits = connectInfiniteHits(Hits);
+
+export const useAlgoliaRefresh = (searchClient) => {
+  const [refreshing, setRefreshing] = useState(false);
+  const refresh = useCallback(() => {
+    if (refreshing) return;
+    // To change 'refresh' on InstantSearch
+    setRefreshing(true);
+    // To manually clear cache
+    if (searchClient && typeof searchClient.clearCache === "function") {
+      searchClient.clearCache();
+    }
+    setRefreshing(false);
+  }, []);
+  return [refresh, { refreshing }];
+};
 
 export default function App() {
+  /**
+   * `useMemo` prevents the searchClient from being recreated on every render.
+   * - Avoids unnecessary XHR requests (see https://tinyurl.com/yyj93r2s).
+   **/
+  const searchClientMemo = useMemo(() => searchClient, []);
+  const [refresh, { refreshing }] = useAlgoliaRefresh(searchClientMemo);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <InstantSearch
+      searchClient={searchClientMemo}
+      refresh={refreshing}
+      indexName="instant_search"
+    >
+      <ConnectedHits onRefresh={refresh} refreshing={refreshing} />
+    </InstantSearch>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
